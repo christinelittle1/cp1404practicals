85147e96b002ffac675d600c2634dd4954c5907a
Christine <christinelitte1@my.jcu.edu.au>
2019-08-20 21:07:41 +1000

    Refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,24 +1,29 @@
 """
-Christine
+Program that asks the user for a password
+and prints asterisks based on its length.
 """
 
 MIN_LENGTH = 6
 
 
 def main():
-    """Get user's password and count length."""
-    password = input("Enter password that contains {} or more characters: ".format(MIN_LENGTH))
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("Enter password that contains {} or more characters: ".format(MIN_LENGTH))
+    """Get password and print asterisks."""
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """"Print number of asterisks equal to password length."""
     print("*" * len(password))
 
 
-def is_valid_password(password):
-    """Check if password is valid."""
-    if len(password) < MIN_LENGTH:
-        return False
-    return True
+def get_password():
+    """Get password and check length is valid."""
+    password = input("Enter password that contains {} or more characters: ".format(MIN_LENGTH))
+    while len(password) < MIN_LENGTH:
+        print("Invalid password!")
+        password = input("Enter password that contains {} or more characters: ".format(MIN_LENGTH))
+    return password
 
 
 main()
