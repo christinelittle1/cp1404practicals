63a188250747823d117f1aad885230893869ffa0
Christine <christinelitte1@my.jcu.edu.au>
2019-08-29 22:27:38 +1000

    Import random. Add constants. Write input line.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,4 +1,14 @@
+"""
+Write a program that asks the user how many "quick
+picks" they wish to generate. The program then generates
+that many lines of output. Each line consists of 6 random
+numbers between 1 and 45.
+"""
+
+import random
+
 MIN_NUMBER = 1
 MAX_NUMBER = 45
+NUMBERS_PER_LINE = 6
 
 lines = int(input("How many quick picks? "))
