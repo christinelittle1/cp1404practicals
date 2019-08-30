4ce011ad40870a068ef6297c74a64e9dbe97745c
Christine <christinelitte1@my.jcu.edu.au>
2019-08-30 14:02:53 +1000

    Add for loops to generate six random numbers for each line of output.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -11,4 +11,11 @@ MIN_NUMBER = 1
 MAX_NUMBER = 45
 NUMBERS_PER_LINE = 6
 
+
 lines = int(input("How many quick picks? "))
+for i in range(lines):
+    quick_picks = []
+    for j in range(NUMBERS_PER_LINE):
+        quick_picks.append(random.randint(MIN_NUMBER, MAX_NUMBER))
+    print(quick_picks)
+
