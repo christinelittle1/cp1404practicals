47399d161933d1a3083169ea31eab6fcc1dd4453
Christine <christinelitte1@my.jcu.edu.au>
2019-09-04 12:26:03 +1000

    Create constant dictionary with colour name and corresponding hexadecimal colour code

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+HEX_COLOUR_CODES = {"black": "#000000", "brown": "#a52a2a", "coral": "#ff7f50",
+                    "magenta": "#ff00ff", "orchid": "#da70d6", "pink": "#ffc0cb",
+                    "SkyBlue": "#87ceeb", "white": "ffffff", "YellowGreen": "9acd32",
+                    "SlateBlue": "#6a5acd"}
+
