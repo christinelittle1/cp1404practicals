734f1a097c97b002c37c0c4b1f94ab5f2480d645
Christine <christinelitte1@my.jcu.edu.au>
2019-09-08 20:41:52 +1000

    Adjust program to sort output in alphabetical order

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,12 +1,13 @@
 word_occurrences = {}
 
 text = input("Text: ")
-
 words = text.split(" ")
+words.sort()
 
 for word in words:
     count = words.count(word)
     word_occurrences[word] = count
 
-print(word_occurrences)
-# print("{}: {}".format(word, word_occurrences))
+for word in word_occurrences:
+    print("{}: {}".format(word, word_occurrences[word]))
+
