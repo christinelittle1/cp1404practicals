21ea10899af4ea9bebefb8324dc35de376bffecc
Christine <christinelitte1@my.jcu.edu.au>
2019-09-08 19:20:27 +1000

    Edit program so lowercase inputs work

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
@@ -15,7 +14,7 @@ while state != "":
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
 
 for state in STATE_NAMES:
     print("{:3} is {}".format(state, STATE_NAMES[state]))
