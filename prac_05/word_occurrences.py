8acc5ef6bd8ea57b7be74a3c329646cd541f5e21
Christine <christinelitte1@my.jcu.edu.au>
2019-09-11 12:57:57 +1000

    Change square brackets to round brackets

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,7 +8,7 @@ for word in words:
     count = words.count(word)
     word_occurrences[word] = count
 
-length_of_longest_word = max([len(word) for word in words])
+length_of_longest_word = max((len(word) for word in words))
 
 for word in word_occurrences:
     print("{:{}} : {}".format(word, length_of_longest_word, word_occurrences[word]))
