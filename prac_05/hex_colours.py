49291721a2266fae0606ba58a52a51604ea858c0
Christine <christinelitte1@my.jcu.edu.au>
2019-09-11 12:42:08 +1000

    Rename constant dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,11 +1,11 @@
-HEX_COLOUR_CODES = {"black": "#000000", "brown": "#a52a2a", "coral": "#ff7f50", "magenta": "#ff00ff",
-                    "orchid": "#da70d6", "pink": "#ffc0cb", "SkyBlue": "#87ceeb", "white": "ffffff",
-                    "YellowGreen": "9acd32", "SlateBlue": "#6a5acd"}
+COLOUR_NAME_TO_HEX_CODE = {"black": "#000000", "brown": "#a52a2a", "coral": "#ff7f50", "magenta": "#ff00ff",
+                      "orchid": "#da70d6", "pink": "#ffc0cb", "SkyBlue": "#87ceeb", "white": "ffffff",
+                      "YellowGreen": "9acd32", "SlateBlue": "#6a5acd"}
 
 colour_name = input("Enter colour name: ")
 while colour_name != "":
-    if colour_name in HEX_COLOUR_CODES:
-        print(HEX_COLOUR_CODES[colour_name])
+    if colour_name in COLOUR_TO_HEX_CODE:
+        print(COLOUR_TO_HEX_CODE[colour_name])
     else:
         print("Invalid colour name")
     colour_name = input("Enter colour name: ")
