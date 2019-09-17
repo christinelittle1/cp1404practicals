0bd5cc81dceaaec57aff3b5a9871a0d30e3eee8a
Christine <christinelitte1@my.jcu.edu.au>
2019-09-17 21:20:13 +1000

    Get guitar age and determine whether it is vintage

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -7,3 +7,12 @@ class Guitar:
 
     def __str__(self):
         return "{} ({}) : ${}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return 2019 - self.year
+
+    def is_vintage(self):
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
