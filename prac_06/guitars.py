443a5bed2cf7afac12101d928995e1a4e41e2b9b
Christine <christinelitte1@my.jcu.edu.au>
2019-09-18 11:47:26 +1000

    Display users guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,12 +2,29 @@ from prac_06.guitar import Guitar
 
 
 def main():
+    guitars = []
+
     print("My guitars!")
     name = input("Name: ")
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    guitar = Guitar(name, year, cost)
-    print(guitar)
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        add_new_guitar = Guitar(name, year, cost)
+        guitars.append(add_new_guitar)
+        print(add_new_guitar)
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print("Guitar {}: {} ({}), worth $ {:,.2f}{}".format(i, guitar.name, guitar.year, guitar.cost,
+                                                             vintage_string))
 
 
 main()
