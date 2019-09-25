3e944ec792e32f85237a5923df9eca2088d56da2
Christine <christinelitte1@my.jcu.edu.au>
2019-09-25 12:51:48 +1000

    Add function that handles up and down increment and function that converts miles to km

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -10,5 +10,13 @@ class ConvertMilesToKilometresApp(App):
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
 
+    def handle_increment(self, increment, value):
+        result = increment + value
+        self.root.ids.input_number.text = str(result)
+
+    def convert_miles_to_km(self, miles):
+        km = miles * 1.609
+        self.root.ids.output_number.text = str(km)
+
 
 ConvertMilesToKilometresApp().run()
