b20dcee308b93a92016a4fca3bbda2971887b5f8
Christine <christinelitte1@my.jcu.edu.au>
2019-10-03 17:30:03 +1000

    Handle invalid inputs

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -1,21 +1,31 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+
+MILES_TO_KM = 1.609
 
 
 class ConvertMilesToKilometresApp(App):
+
     def build(self):
-        Window.size = (400, 200)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
 
-    def handle_increment(self, increment, value):
-        result = increment + value
+    @staticmethod
+    def handle_invalid_input(text):
+        try:
+            miles = float(text)
+            return miles
+        except ValueError:
+            return 0.0
+
+    def handle_increment(self, increment, text):
+        result = increment + self.handle_invalid_input(text)
         self.root.ids.input_number.text = str(result)
 
-    def convert_miles_to_km(self, miles):
-        km = miles * 1.609
+    def convert_miles_to_km(self, text):
+        miles = self.handle_invalid_input(text)
+        km = miles * MILES_TO_KM
         self.root.ids.output_number.text = str(km)
 
 
