ebc1f4bbae3931c53fe26345dc0342c46d97d370
Christine <christinelitte1@my.jcu.edu.au>
2019-10-17 20:00:53 +1000

    Test methods in Taxi class

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,8 +2,19 @@ from prac_08.taxi import Taxi
 
 
 def main():
+    # 1.
     new_taxi = Taxi("Prius 1", 100, 1.23)
+    # 2.
+    new_taxi.drive(40)
+    # 3.
     print(new_taxi)
+    print("current fare = ${}".format(new_taxi.get_fare()))
+    # 4.
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    # 5.
+    print(new_taxi)
+    print("current fare = ${}".format(new_taxi.get_fare()))
 
 
 main()
