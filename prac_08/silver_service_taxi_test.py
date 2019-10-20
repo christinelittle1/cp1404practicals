7350d88d315a4e6425ee0f5a8ffc24237da17964
Christine <christinelitte1@my.jcu.edu.au>
2019-10-20 12:14:30 +1000

    Test get_fare method for SilverServiceTaxi class

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,11 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    new_silver_service_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+    new_silver_service_taxi.drive(18)
+    print(new_silver_service_taxi)
+    print("get_fare() - Expected: $48.78. Got: ${:.2f}".format(new_silver_service_taxi.get_fare()))
+
+
+main()
