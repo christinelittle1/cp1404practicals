f8db221e8d35f13629df312c8491e7d8dcdbcb63
Christine <christinelitte1@my.jcu.edu.au>
2019-10-20 12:22:42 +1000

    Update get_fare() method in Taxi to use round() and test

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -5,7 +5,11 @@ def main():
     new_silver_service_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
     new_silver_service_taxi.drive(18)
     print(new_silver_service_taxi)
-    print("get_fare() - Expected: $48.78. Got: ${:.2f}".format(new_silver_service_taxi.get_fare()))
+    # Without round()
+    # print("get_fare() - Expected: $48.78. Got: ${:.2f}".format(new_silver_service_taxi.get_fare()))
+
+    # With round()
+    print("get_fare() - Expected: $48.80. Got: ${:.2f}".format(new_silver_service_taxi.get_fare()))
 
 
 main()
