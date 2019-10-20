4d31f621ad84f067ba9db97392c3be35f202aedb
Christine <christinelitte1@my.jcu.edu.au>
2019-10-20 12:13:16 +1000

    Create overridden __str__ and get_fare method

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -2,8 +2,15 @@ from prac_08.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
 
-    def __init__(self, name, fuel, fanciness=0.0):
+    def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
