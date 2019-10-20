8caabfa2521bf15990ef20b5bfcf03d78a652d91
Christine <christinelitte1@my.jcu.edu.au>
2019-10-20 12:34:40 +1000

    Initial commit

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,23 @@
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+MENU = """q)uit, c)hoose taxi, d)rive"""
+
+
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's Drive!")
+    print(MENU)
+    choice = input(">>> ")
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ")
+
+
+main()
