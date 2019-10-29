23f0d54924497938991e04e2af26faccc288ff88
Christine <christinelitte1@my.jcu.edu.au>
2019-10-29 18:12:08 +1000

    Categorise extensions

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -5,26 +5,21 @@ import shutil
 def main():
     os.chdir('FilesToSort')
 
-    try:
-        os.mkdir('Spreadsheets')
-        os.mkdir('Images')
-        os.mkdir('Docs')
-    except FileExistsError:
-        pass
-
-    extension_to_category = {'doc': '', 'docx': '', 'png': '', 'gif': '', 'txt': '', 'xls': '', 'xlsx': '', 'jpg': ''}
-
+    extension_to_category = {}
     for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
-    categories = []
-    for extension in extension_to_category:
-        category = input("What category would you like to sort {} files into? ".format(extension))
-        if category not in categories:
-            categories.append(category)
-        extension_to_category[extension] = category
-    print(categories)
+
+        extension = filename.split('.')[-1]
+        if extension not in extension_to_category:
+            category = input("What category would you like to sort {} files into? ".format(extension))
+            extension_to_category[extension] = category
+            try:
+                os.mkdir(category)
+            except FileExistsError:
+                pass
     print(extension_to_category)
+
 
 
 main()
